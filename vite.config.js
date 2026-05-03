f12167a1ac0d9a818923d49be87eae7627499ffb
Rodrigo7-dev <146425633+Rodrigo7-dev@users.noreply.github.com>
2026-05-03 22:33:57 +0100

    mais um feito

--- vite.config.js
+++ vite.config.js
@@ -9,34 +9,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['vue', 'vue-router', 'pinia', 'axios']
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    assetsDir: 'assets',
-    sourcemap: false
-  },
-
-  server: {
-    hmr: {
-      host: 'localhost',
-      port: 5173
-    }
-  },
-
-  base: '/',
-  preview: {
-    port: 4173
   }
 })
