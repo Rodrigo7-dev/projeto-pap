001c5f6558806af3d25581300f547bcef973e4b3
Rodrigo7-dev <146425633+Rodrigo7-dev@users.noreply.github.com>
2026-05-03 22:29:39 +0100

    mais um feito

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  alias: {
+    '@': path.resolve(__dirname, './src')
+  }
+},
   build: {
     rollupOptions: {
       output: {
